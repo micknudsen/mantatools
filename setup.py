1d288694958bceda42c185f905aa76137fd4aaa9
Michael Knudsen <michaelk@clin.au.dk>
2024-02-19 19:31:23 +0100

    Hello, World!

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mantatools",
+    version="0.0",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    test_suite="tests",
+    entry_points={"console_scripts": ["mantatools = mantatools.client:run"]},
+    python_requires=">=3.12",
+    author="Michael Knudsen",
+    author_email="micknudsen@gmail.com",
+)
