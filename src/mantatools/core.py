11b932c63534e9028935eee2b426cabe7b45a784
Michael Knudsen <michaelk@clin.au.dk>
2024-02-20 09:19:57 +0100

    Implemented get_info method in Variant class

--- src/mantatools/core.py
+++ src/mantatools/core.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Dict, List, Union
+
+from mantatools.exceptions import InfoFieldNotFound
 
 
 @dataclass
@@ -14,3 +16,18 @@ class Variant:
     info: str
     format: str
     genotypes: List[str]
+
+    def __post_init__(self) -> None:
+        self.info_dict: Dict[str, Union[str, bool]] = {}
+        for entry in self.info.split(";"):
+            if "=" in entry:
+                key, value = entry.split("=", 1)
+                self.info_dict[key] = value
+            else:
+                self.info_dict[entry] = True
+
+    def get_info(self, key: str) -> Union[str, bool]:
+        try:
+            return self.info_dict[key]
+        except KeyError:
+            raise InfoFieldNotFound()
