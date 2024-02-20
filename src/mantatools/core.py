6603ad8c893b89ce13e51a43b5642c46ebf3d237
Michael Knudsen <michaelk@clin.au.dk>
2024-02-20 09:22:22 +0100

    Implemented get_genotype method in Variant class

--- src/mantatools/core.py
+++ src/mantatools/core.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Dict, List, Union
 
-from mantatools.exceptions import InfoFieldNotFound
+from mantatools.exceptions import GenotypeFieldNotFound, InfoFieldNotFound
 
 
 @dataclass
@@ -18,7 +18,10 @@ class Variant:
     genotypes: List[str]
 
     def __post_init__(self) -> None:
+
         self.info_dict: Dict[str, Union[str, bool]] = {}
+        self.genotype_dicts: List[Dict[str, str]] = []
+
         for entry in self.info.split(";"):
             if "=" in entry:
                 key, value = entry.split("=", 1)
@@ -26,8 +29,20 @@ class Variant:
             else:
                 self.info_dict[entry] = True
 
+        for genotype in self.genotypes:
+            genotype_dict = {}
+            for i, key in enumerate(self.format.split(":")):
+                genotype_dict[key] = genotype.split(":")[i]
+            self.genotype_dicts.append(genotype_dict)
+
     def get_info(self, key: str) -> Union[str, bool]:
         try:
             return self.info_dict[key]
         except KeyError:
             raise InfoFieldNotFound()
+
+    def get_genotype(self, key: str, sample: int = 0) -> str:
+        try:
+            return self.genotype_dicts[sample][key]
+        except KeyError:
+            raise GenotypeFieldNotFound()
