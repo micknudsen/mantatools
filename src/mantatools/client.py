c4ce403b0f5c96b015d9f5059f73121e8394f188
Michael Knudsen <michaelk@clin.au.dk>
2024-02-21 17:58:24 +0100

    Implemented create_contigs_fastq command

--- src/mantatools/client.py
+++ src/mantatools/client.py
@@ -2,6 +2,7 @@ import gzip
 
 import click
 
+from mantatools.exceptions import InfoFieldNotFound
 from mantatools.parsers import parse_vcf
 
 
@@ -12,10 +13,22 @@ def client():
 
 @client.command()
 @click.option("--vcf", type=click.Path(exists=True), required=True)
-def create_bedpe(vcf: str):
+def create_bedpe(vcf: str) -> None:
     with gzip.open(vcf, "rt") as stream:
         for variant in parse_vcf(stream).values():
             print(variant.to_bedpe())
+
+
+@client.command()
+@click.option("--vcf", type=click.Path(exists=True), required=True)
+def create_contigs_fastq(vcf: str) -> None:
+    with gzip.open(vcf, "rt") as stream:
+        for variant in parse_vcf(stream).values():
+            try:
+                contig = str(variant.get_info("CONTIG"))
+                print("\n".join([f"@{variant.id}", contig, "+", "I" * len(contig)]))
+            except InfoFieldNotFound:
+                pass
 
 
 def run():
