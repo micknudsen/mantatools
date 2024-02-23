3dcdeb2007f2e767e90128a2f1f3404f60053073
Michael Knudsen <michaelk@clin.au.dk>
2024-02-23 09:48:41 +0100

    Improved check_conting_support function

--- src/mantatools/validation.py
+++ src/mantatools/validation.py
@@ -6,4 +6,6 @@ from mantatools.core import Variant
 
 
 def check_contig_support(variant: Variant, alignments: List[AlignedSegment]) -> bool:
-    return True
+    """Check if the variant is supported by alignment of contigs. This
+    is very much work in progress and should not yet be used in production!"""
+    return len(alignments) > 0
