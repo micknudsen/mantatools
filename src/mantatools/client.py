b9e5458f8038b860b6b9944f4082efcc2b32da11
Michael Knudsen <michaelk@clin.au.dk>
2024-02-28 15:33:42 +0100

    Added --include_fields command line option

--- src/mantatools/client.py
+++ src/mantatools/client.py
@@ -3,7 +3,7 @@ import gzip
 import click
 
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from pysam import AlignedSegment, AlignmentFile
 
@@ -19,10 +19,15 @@ def client():
 
 @client.command()
 @click.option("--vcf", type=click.Path(exists=True), required=True)
-def create_bedpe(vcf: str) -> None:
+@click.option("--include_fields", type=str, required=False)
+def create_bedpe(vcf: str, include_fields: Optional[str] = None) -> None:
     with gzip.open(vcf, "rt") as stream:
         for variant in parse_vcf(stream).values():
-            print(variant.to_bedpe())
+            print(
+                variant.to_bedpe(
+                    include_fields=include_fields.split(",") if include_fields else None
+                )
+            )
 
 
 @client.command()
