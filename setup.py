5a29574ece05688b3d97b81a0db86e92806404ad
Michael Knudsen <michaelk@clin.au.dk>
2024-03-05 08:19:20 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mantatools",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages("src"),
     package_dir={"": "src"},
     test_suite="tests",
