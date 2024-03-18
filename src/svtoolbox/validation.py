f3a2f555ae29a853d15524792be686e1e7a1d0d2
Michael Knudsen <michaelk@clin.au.dk>
2024-03-18 19:31:37 +0100

    Change of plans

--- src/svtoolbox/validation.py
+++ src/svtoolbox/validation.py
@@ -8,28 +8,4 @@ from svtoolbox.core import Variant
 def check_contig_support(variant: Variant, alignments: List[AlignedSegment]) -> bool:
     """Check if the variant is supported by alignment of contigs. This
     is very much work in progress and should not yet be used in production!"""
-
-    # The contig does not even map to the refrence.
-    if len(alignments) == 0:
-        return False
-
-    # The contig maps to the reference, and there is only the primary alignment.
-    elif len(alignments) == 1:
-
-        cigartuples = alignments[0].cigartuples or []
-
-        match variant.get_info(key="SVTYPE"):
-
-            case "INS":
-                return (1, abs(int(variant.get_info(key="SVLEN")))) in cigartuples
-
-            case "DEL":
-                return (2, abs(int(variant.get_info(key="SVLEN")))) in cigartuples
-
-            case _:
-                return False
-
-    # The contig maps to the reference, and there is a least
-    # one supplementary alignment.
-    else:
-        return True
+    return True
