82852c3e87252f59db3c8d2177ebaa662407925d
Michael Knudsen <michaelk@clin.au.dk>
2024-05-07 10:46:12 +0200

    Rolling back. Bug is not here.

--- tests/test_core.py
+++ tests/test_core.py
@@ -48,6 +48,57 @@ class TestVariant(unittest.TestCase):
         with self.assertRaises(GenotypeFieldNotFound):
             self.variant.get_genotype("NON_EXISTENT_GENOTYPE_FIELD")
 
+    def test_to_bedpe(self) -> None:
+        self.assertEqual(
+            self.variant.to_bedpe(),
+            BedPE(
+                chrom_1="chr1",
+                start_1=89,
+                end_1=105,
+                chrom_2="chr1",
+                start_2=184,
+                end_2=220,
+                name="MyVariant",
+                score="1000",
+                strand_1=None,
+                strand_2=None,
+            ),
+        )
+
+    def test_to_bedpe_with_extra_fields(self) -> None:
+        self.assertEqual(
+            self.variant.to_bedpe(
+                include_fields=[
+                    "REF",
+                    "ALT",
+                    "QUAL",
+                    "FILTER",
+                ]
+            ),
+            BedPE(
+                chrom_1="chr1",
+                start_1=89,
+                end_1=105,
+                chrom_2="chr1",
+                start_2=184,
+                end_2=220,
+                name="MyVariant",
+                score="1000",
+                strand_1=None,
+                strand_2=None,
+                fields={
+                    "REF": "A",
+                    "ALT": "<DEL>",
+                    "QUAL": "1000",
+                    "FILTER": "PASS",
+                },
+            ),
+        )
+
+    def test_to_bedpe_field_not_found(self) -> None:
+        with self.assertRaises(FieldNotFound):
+            self.variant.to_bedpe(include_fields=["NON_EXISTENT_FIELD"])
+
     def test_str_method(self) -> None:
         self.assertEqual(
             self.variant.__str__(),
@@ -370,114 +421,4 @@ class TestBedPE(unittest.TestCase):
         self.assertEqual(
             bedpe.__str__(),
             "chr1\t100\t200\tchr3\t400\t500\tMyVariant\t1000\t+\t-\tREF=A;ALT=<DEL>;QUAL=1000;FILTER=PASS",
-        )
-
-
-class TestToBedPE(unittest.TestCase):
-    def setUp(self) -> None:
-        self.variant = Variant(
-            chrom="chr1",
-            pos="100",
-            id="MyVariant",
-            ref="A",
-            alt="<DEL>",
-            qual="1000",
-            filter="PASS",
-            info="IMPRECISE;END=200;SVTYPE=DEL;CIPOS=-10,5;CIEND=-15,20",
-            format="GT:PR",
-            genotypes=["0/1:20,15"],
-        )
-
-    def test_to_bedpe(self) -> None:
-        self.assertEqual(
-            self.variant.to_bedpe(),
-            BedPE(
-                chrom_1="chr1",
-                start_1=89,
-                end_1=105,
-                chrom_2="chr1",
-                start_2=184,
-                end_2=220,
-                name="MyVariant",
-                score="1000",
-                strand_1=None,
-                strand_2=None,
-            ),
-        )
-
-    def test_to_bedpe_extra_fields(self) -> None:
-        self.assertEqual(
-            self.variant.to_bedpe(
-                include_fields=[
-                    "REF",
-                    "ALT",
-                    "QUAL",
-                    "FILTER",
-                ]
-            ),
-            BedPE(
-                chrom_1="chr1",
-                start_1=89,
-                end_1=105,
-                chrom_2="chr1",
-                start_2=184,
-                end_2=220,
-                name="MyVariant",
-                score="1000",
-                strand_1=None,
-                strand_2=None,
-                fields={
-                    "REF": "A",
-                    "ALT": "<DEL>",
-                    "QUAL": "1000",
-                    "FILTER": "PASS",
-                },
-            ),
-        )
-
-    def test_to_bedpe_field_not_found(self) -> None:
-        with self.assertRaises(FieldNotFound):
-            self.variant.to_bedpe(include_fields=["NON_EXISTENT_FIELD"])
-
-    def test_to_bedpe_bnd_variant(self) -> None:
-        variant = Variant(
-            chrom="chr5",
-            pos="500",
-            id="MantaBND:0",
-            ref="A",
-            alt="]chr6:600]A",
-            qual="1000",
-            filter="PASS",
-            info="SVTYPE=BND;MATEID=MantaBND:1",
-            format="GT",
-            genotypes=["1/1"],
-        )
-
-        variant.mate = Variant(
-            chrom="chr6",
-            pos="600",
-            id="MantaBND:1",
-            ref="C",
-            alt="C[chr5:500[",
-            qual="1000",
-            filter="PASS",
-            info="SVTYPE=BND;MATEID=MantaBND:0",
-            format="GT",
-            genotypes=["1/1"],
-        )
-
-        self.assertEqual(
-            variant.to_bedpe(),
-            BedPE(
-                chrom_1="chr5",
-                start_1=499,
-                end_1=500,
-                chrom_2="chr6",
-                start_2=599,
-                end_2=600,
-                name="MantaBND:0",
-                score="1000",
-                strand_1=None,
-                strand_2=None,
-            ),
         )
