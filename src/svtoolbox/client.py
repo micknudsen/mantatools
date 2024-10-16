49f9eb38414d89475b6a7f0c1f6d43fdd2b81245
Michael Knudsen <michaelk@clin.au.dk>
2024-10-16 10:24:51 +0200

    Maybe one day...

--- src/svtoolbox/client.py
+++ src/svtoolbox/client.py
@@ -9,7 +9,6 @@ from pysam import AlignedSegment, AlignmentFile
 
 from svtoolbox.exceptions import InfoFieldNotFound, SVToolBoxException
 from svtoolbox.parser import parse_vcf
-from svtoolbox.validation import check_contig_support
 
 
 @click.group()
@@ -40,44 +39,6 @@ def create_contigs_fastq(vcf: str) -> None:
                 print("\n".join([f"@{variant.id}", contig, "+", "I" * len(contig)]))
             except InfoFieldNotFound:
                 pass
-
-
-@client.command()
-@click.option("--vcf", type=click.Path(exists=True), required=True)
-@click.option("--bam", type=click.Path(exists=True), required=True)
-def validate_variants(vcf: str, bam: str) -> None:
-    alignments: Dict[str, List[AlignedSegment]] = defaultdict(list)
-    for alignment in AlignmentFile(bam, "rb"):
-        if alignment.query_name is None:
-            raise SVToolBoxException("Missing query name in alignment")
-        alignments[alignment.query_name].append(alignment)
-
-    with gzip.open(vcf, "rt") as stream:
-        variants = parse_vcf(stream)
-
-    added_info_field = False
-    with gzip.open(vcf, "rt") as f:
-        for line in f.read().splitlines():
-            # This is a header line
-            if line.startswith("#"):
-                # Add the SUPPORTED INFO field definition together with
-                # the definitons of the other INFO fields.
-                if line.startswith("##INFO") and not added_info_field:
-                    print(
-                        '##INFO=<ID=SUPPORTED,Number=0,Type=Flag,Description="Supported by contig breakpoints">'
-                    )
-                    added_info_field = True
-                # Keep all existing header lines
-                print(line)
-                continue
-            # We have reached the end of the header
-            break
-
-    for variant in variants.values():
-        # Add SUPPORTED flag if the variant is supported by contig breakpoints
-        if check_contig_support(variant=variant, alignments=alignments[variant.id]):
-            variant.set_info(key="SUPPORTED", value=True)
-        print(variant)
 
 
 def run():
