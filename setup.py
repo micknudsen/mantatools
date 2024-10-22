8c9f08cee7e6cbabd3f206e4e68273941edebbf8
Michael Knudsen <michaelk@clin.au.dk>
2024-10-22 07:56:18 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="svtoolbox",
-    version="v1.1",
+    version="v1.2",
     packages=find_packages("src"),
     package_dir={"": "src"},
     test_suite="tests",
